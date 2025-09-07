b7881d6f87f173ed2c5ca6471bba877ca0880f2b
Wajid002 <syedwajid002@gmail.com>
2025-09-07 19:41:06 +0530

    Updating Projects Section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
   }, []);
 
   const navigation = [
-    { name: 'Home', href: '#home' },
+    { name: 'Home', href: '/' },
     {
       name: 'About',
       href: '#about',
